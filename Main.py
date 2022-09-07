5bcafcd8336f323746077eb57d963befc68338ce
Bipradip-Chakraborty <100284493+Bipradip-Chakraborty@users.noreply.github.com>
2022-09-07 18:58:02 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,15 @@
+print('Welcome to calculator !! \n For calculation enter number and operator in this sequence a + b put one space between each set of numbers.\n For addition use : a + b \n For subtraction : a - b \n For multiplication : a x b \n For division : a / b')
+
+num1, operator, num2 = input('= ').split()
+if operator == '+':
+    sum = int(num1)+int(num2)
+    print(sum)
+elif operator == '-':
+    subtract = int(num1)-int(num2)
+    print(subtract)
+elif operator == 'x':
+    multiply = int(num1)*int(num2)
+    print(multiply)
+elif operator == '/':
+    divide = int(num1)/int(num2)
+    print(divide)
